0e821e9ce2715e86b73c2fd692e9caedfa8558d7
Ryan Atkinson <mail@ryanatkn.com>
2023-06-29 19:45:14 -0600

    cleanup

--- src/routes/FeltFooter.svelte
+++ src/routes/FeltFooter.svelte
@@ -4,8 +4,7 @@
 
 <footer {...$$restProps} class="prose panel padded_lg">
 	<a href="https://www.felt.dev/">felt.dev</a>
-	<br />
-	<a href="https://github.com/feltjs/felt-mural" rel="me"><GithubLogo /></a>
+	<a class="github" href="https://github.com/feltjs/felt-mural" rel="me"><GithubLogo /></a>
 </footer>
 
 <style>
@@ -15,13 +14,14 @@
 		align-items: center;
 		flex-direction: column;
 	}
-	.social a {
+	.github {
 		--border_width: var(--border_width_4);
 		--border_color: transparent;
 		text-decoration: none;
 		display: flex;
 		border-radius: 50%;
 		border: var(--border_width) double var(--border_color);
+		margin-top: var(--spacing_xs);
 	}
 	.social a:hover {
 		--border_color: var(--color_1);
