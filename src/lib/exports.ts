9102e0868d68a55210082cef2082a87c3839bd58
Ryan Atkinson <mail@ryanatkn.com>
2023-09-18 07:27:41 -0600

    wip

--- src/lib/exports.ts
+++
@@ -1,9 +0,0 @@
-export const exports = [
-	'lib/item.ts',
-	'lib/Mural.svelte',
-	'lib/MuralItem.svelte',
-	'lib/MuralItemList.svelte',
-	'lib/MuralItemListItem.svelte',
-	'lib/Scaled.svelte',
-	'lib/Surface.svelte',
-];
