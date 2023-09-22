5f6cc5b442fbd1021106d1db14bdc6d785e2cb62
Ryan Atkinson <mail@ryanatkn.com>
2023-09-21 23:49:05 -0600

    add library dep

--- src/routes/library/LibraryVocab.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import {base} from '$app/paths';
-
-	// TODO add contextmenu behavior
-
-	export let name: string; // TODO type, generate from `libraryItems`?
-</script>
-
-<code {...$$restProps}><a href="{base}/library/{name}"><slot>{name}</slot></a></code>
