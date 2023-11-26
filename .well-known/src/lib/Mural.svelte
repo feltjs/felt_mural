ac8b1bc6bfae36d9482fb591bd22a3c91122874c
Ryan Atkinson <mail@ryanatkn.com>
2023-11-25 20:51:38 -0700

    deployment

--- .well-known/src/lib/Mural.svelte
+++ .well-known/src/lib/Mural.svelte
@@ -35,66 +35,66 @@
 	export let pointer_y: number | undefined = undefined;
 	export let scale: number | undefined = undefined;
 
-	const createItem = (): Svg_Item | null => {
-		const itemData =
-			selectedBrush === 'pen' || selectedBrush === 'polyline'
+	const create_item = (): Svg_Item | null => {
+		const item_data =
+			selected_brush === 'pen' || selected_brush === 'polyline'
 				? create_polyline()
-				: selectedBrush === 'circle'
+				: selected_brush === 'circle'
 				  ? create_circle(pointer_x, pointer_y)
 				  : null;
-		if (!itemData) return null;
-		return itemData;
+		if (!item_data) return null;
+		return item_data;
 	};
 
 	// hooks that can be overridden or externally bound for calling
-	export let addItem: (
+	export let add_item: (
 		list: Array<Writable<Svg_Item>>,
 		item: Writable<Svg_Item>,
 		id: Item_Id,
 	) => Array<Writable<Svg_Item>> = (list, item) => list.concat(item);
-	export let updateItem: <TItem extends Svg_Item>(
+	export let update_item: <TItem extends Svg_Item>(
 		item: Writable<TItem>,
 		data: Partial<TItem>,
 	) => void = (item, data) => update_item_data(item, data as any); // TODO type hack
-	export let selectItemToDraw: (
+	export let select_item_to_draw: (
 		list: Array<Writable<Svg_Item>>,
-	) => Writable<Svg_Item> | undefined = (list) => $itemSelection || list.at(-1);
-	export let removeItem: (
+	) => Writable<Svg_Item> | undefined = (list) => $item_selection || list.at(-1);
+	export let remove_item: (
 		list: Array<Writable<Svg_Item>>,
 		item: Writable<Svg_Item>,
 		id: Item_Id,
 	) => Array<Writable<Svg_Item>> = (list, item): Array<Writable<Svg_Item>> =>
 		list.filter((e) => e !== item);
-	export let removeAllItems: (list: Array<Writable<Svg_Item>>) => Array<Writable<Svg_Item>> = (
+	export let remove_all_items: (list: Array<Writable<Svg_Item>>) => Array<Writable<Svg_Item>> = (
 		_list,
 	) => [];
 
-	const itemsById = new Map<string, Writable<Svg_Item>>();
+	const items_by_id = new Map<string, Writable<Svg_Item>>();
 
-	export const handleAction = (action: Mural_Action): void => {
+	export const handle_action = (action: Mural_Action): void => {
 		switch (action.type) {
-			case 'addItem': {
+			case 'add_item': {
 				const item = writable(action.item);
-				itemsById.set(action.item.id, item);
-				items = addItem(items, item, action.item.id);
+				items_by_id.set(action.item.id, item);
+				items = add_item(items, item, action.item.id);
 				break;
 			}
-			case 'updateItem': {
-				const item = itemsById.get(action.id);
+			case 'update_item': {
+				const item = items_by_id.get(action.id);
 				if (!item) return;
-				updateItem(item, action.data);
+				update_item(item, action.data);
 				break;
 			}
-			case 'removeItem': {
-				const item = itemsById.get(action.id);
+			case 'remove_item': {
+				const item = items_by_id.get(action.id);
 				if (!item) return;
-				itemsById.delete(action.id);
-				items = removeItem(items, item, action.id);
+				items_by_id.delete(action.id);
+				items = remove_item(items, item, action.id);
 				break;
 			}
-			case 'removeAllItems': {
-				itemsById.clear();
-				items = removeAllItems(items);
+			case 'remove_all_items': {
+				items_by_id.clear();
+				items = remove_all_items(items);
 				break;
 			}
 		}
@@ -102,64 +102,64 @@
 
 	const act = (action: Mural_Action): any => {
 		dispatch('action', action);
-		handleAction(action);
+		handle_action(action);
 	};
 
 	// TODO where does this belog? might need to add `mural.ts`
-	type BrushType = 'pen' | 'polyline' | 'circle';
+	type Brush_Type = 'pen' | 'polyline' | 'circle';
 
 	// other options
-	export let brushes: BrushType[] = ['pen', 'polyline', 'circle'];
-	export let selectedBrush: BrushType = brushes[0];
-	export let itemSelection: Writable<Writable<Svg_Item> | null> = writable(null);
+	export let brushes: Brush_Type[] = ['pen', 'polyline', 'circle'];
+	export let selected_brush: Brush_Type = brushes[0];
+	export let item_selection: Writable<Writable<Svg_Item> | null> = writable(null);
 
 	$: if (pointer_down) {
-		startDrawing();
+		start_drawing();
 	} else if (pointer_down === false) {
 		// ignore `undefined`
-		stopDrawing();
+		stop_drawing();
 	}
 	let drawing = false;
-	const startDrawing = () => {
+	const start_drawing = () => {
 		if (drawing) return;
 		// TODO these probably won't stay simple, but if they do we could bind `pointer_down` directly to `drawing`
 		drawing = true;
-		if ($itemSelection) return;
-		const item = createItem();
+		if ($item_selection) return;
+		const item = create_item();
 		if (!item) return;
-		act({type: 'addItem', item});
+		act({type: 'add_item', item});
 		// TODO refactor this, very gross
-		if (selectedBrush === 'polyline') {
-			$itemSelection = items.find((e) => get(e).id === item.id) || null;
+		if (selected_brush === 'polyline') {
+			$item_selection = items.find((e) => get(e).id === item.id) || null;
 		}
 	};
-	const stopDrawing = () => {
+	const stop_drawing = () => {
 		if (!drawing) return;
 		drawing = false;
 	};
 
 	$: if (drawing && pointer_x !== undefined && pointer_y !== undefined)
-		drawAt(pointer_x, pointer_y);
-	const drawAt = (x: number, y: number) => {
-		const item = selectItemToDraw(items);
+		draw_at(pointer_x, pointer_y);
+	const draw_at = (x: number, y: number) => {
+		const item = select_item_to_draw(items);
 		if (!item) return; // we expect an item but just in case
 		const {id, type} = get(item);
-		// TODO seems like this belongs in `updateItem`
+		// TODO seems like this belongs in `update_item`
 		if (type === 'polyline') {
 			act({
-				type: 'updateItem',
+				type: 'update_item',
 				id,
-				data: {appendPoints: [parsers.x(x), parsers.y(y)]},
+				data: {append_points: [parsers.x(x), parsers.y(y)]},
 			});
 			// TODO ideally wouldn't have to cast the type here or use `get` above
 		} // else unhandled type
 	};
 
-	$: enableBrushes = $itemSelection === null;
+	$: enable_brushes = $item_selection === null;
 
-	$: selectedItem = $itemSelection;
-	$: selectedPolylinePointsData =
-		$selectedItem?.type === 'polyline' ? to_points_data($selectedItem) : undefined;
+	$: selected_item = $item_selection;
+	$: selected_polyline_points_data =
+		$selected_item?.type === 'polyline' ? to_points_data($selected_item) : undefined;
 </script>
 
 <div
@@ -185,14 +185,14 @@
 					{#each items as item (item)}
 						<Mural_Item {item} />
 					{/each}
-					{#if pointing && !pointer_down && selectedBrush === 'polyline' && $selectedItem?.type === 'polyline' && selectedPolylinePointsData?.length}
+					{#if pointing && !pointer_down && selected_brush === 'polyline' && $selected_item?.type === 'polyline' && selected_polyline_points_data?.length}
 						<line
-							x1={selectedPolylinePointsData.at(-2)}
-							y1={selectedPolylinePointsData.at(-1)}
+							x1={selected_polyline_points_data.at(-2)}
+							y1={selected_polyline_points_data.at(-1)}
 							x2={pointer_x}
 							y2={pointer_y}
-							stroke={$selectedItem.stroke ?? DEFAULT_POLYLINE_STROKE}
-							stroke-width={$selectedItem.strokeWidth ?? DEFAULT_POLYLINE_STROKE_WIDTH}
+							stroke={$selected_item.stroke ?? DEFAULT_POLYLINE_STROKE}
+							stroke-width={$selected_item.stroke_width ?? DEFAULT_POLYLINE_STROKE_WIDTH}
 						/>
 					{/if}
 				</svg>
@@ -203,10 +203,10 @@
 		<div class="row">
 			{#each brushes as brush (brush)}
 				<button
-					class:selected={enableBrushes && selectedBrush === brush}
+					class:selected={enable_brushes && selected_brush === brush}
 					on:click={() => {
-						$itemSelection = null;
-						selectedBrush = brush;
+						$item_selection = null;
+						selected_brush = brush;
 					}}
 				>
 					{brush}
@@ -215,15 +215,15 @@
 		</div>
 		<button
 			on:click={() => {
-				$itemSelection = null;
-				act({type: 'removeAllItems'});
+				$item_selection = null;
+				act({type: 'remove_all_items'});
 			}}
 			disabled={!items.length}
 		>
 			clear all
 		</button>
 	</div>
-	<Mural_Item_List {items} on:action={(e) => act(e.detail)} {itemSelection} />
+	<Mural_Item_List {items} on:action={(e) => act(e.detail)} {item_selection} {width} {height} />
 </div>
 
 <style>
